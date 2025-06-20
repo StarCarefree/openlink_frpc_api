2d82d1218761b84755485d7bb71329f79b1375c4
Terry-MC123 <terry_mc@qq.com>
2025-06-20 20:08:48 +0800

    fix

--- quilt/src/main/java/fun/moystudio/openlink/frpc/quilt/OpenLinkFrpcApiQuilt.java
+++ quilt/src/main/java/fun/moystudio/openlink/frpc/quilt/OpenLinkFrpcApiQuilt.java
@@ -1,13 +1,14 @@
 package fun.moystudio.openlink.frpc.quilt;
 
+import net.fabricmc.api.ModInitializer;
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+
 
 import fun.moystudio.openlink.frpc.OpenLinkFrpcApi;
 
 public final class OpenLinkFrpcApiQuilt implements ModInitializer {
     @Override
-    public void onInitialize(ModContainer mod) {
+    public void onInitialize() {
         // Run our common setup.
         OpenLinkFrpcApi.init();
     }
